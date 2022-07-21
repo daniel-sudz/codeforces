cafc4162744fbf79b5c3ab5b71868702c60566b0
Daniel Sudzilouski <daniel@sudzilouski.com>
2022-07-21 10:28:27 -0700

    Create codeforces.cpp

---
+++ skeleton/codeforces.cpp
@@ -0,0 +1,21 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pi pair<int, int>
+#define vpi vector<pair<int, int>>
+#define endl '\n'
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#ifdef ONLINE_JUDGE
+    # define watch(x) {}
+    # define printv(n) {}
+#else
+    #define watch(x) {cerr<<(#x)<<" is "<<(x)<<endl;}
+    #define printv(n) {for(auto v:n) cerr<<v<<" ";cerr<<endl;}
+#endif
+
+int main() {
+    fastio
+    cout << "done" << endl;
+}
