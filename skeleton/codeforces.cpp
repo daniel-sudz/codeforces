7ad646864ff271ac6ba3775ce224c3e4db3d9260
Daniel Sudzilouski <daniel@sudzilouski.com>
2022-08-02 15:29:49 -0700

    Update codeforces.cpp

--- skeleton/codeforces.cpp
+++ skeleton/codeforces.cpp
@@ -1,21 +1,28 @@
 #include "bits/stdc++.h"
-using namespace std;
+
 #define ll long long
+#define pb push_back
 #define vi vector<int>
+#define vb vector<bool>
+#define vll vector<ll>
 #define vvi vector<vector<int>>
-#define pi pair<int, int>
-#define vpi vector<pair<int, int>>
+#define vvll vector<vector<ll>>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vpii vector<pair<int, int>>
+#define vpll vector<pair<ll,ll>>
 #define endl '\n'
 #define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 #ifdef ONLINE_JUDGE
     # define watch(x) {}
     # define printv(n) {}
 #else
-    #define watch(x) {cerr<<(#x)<<" is "<<(x)<<endl;}
-    #define printv(n) {for(auto v:n) cerr<<v<<" ";cerr<<endl;}
+#define watch(x) {cerr<<(#x)<<" is "<<(x)<<endl;}
+#define printv(n) {for(auto v:n) cerr<<v<<" ";cerr<<endl;}
 #endif
+using namespace std;
 
-int main() {
+
+int main () {
     fastio
-    cout << "done" << endl;
 }
