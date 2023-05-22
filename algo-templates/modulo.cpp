e0de65158d8622ce079abf240b9e1acdd123a9b0
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-05-22 16:14:24 -0400

    Create modulo.cpp

---
+++ algo-templates/modulo.cpp
@@ -0,0 +1,21 @@
+/* Fast exponent and Fermat's Little Theorem for modular inverse */
+ 
+ll exp(ll base, ll exp, ll MOD) {
+    vll nums = {base};
+    while((1ll<<(ll)(nums.size()-1))<exp) {
+        nums.pb(((nums.back())*nums.back())%MOD);
+    }
+    ll ans = 1ll; 
+    while(exp > 0) {
+        if(exp >= (1ll<<(ll)(nums.size()-1))) {
+            exp -= (1ll<<(ll)(nums.size()-1));
+            ans = (ans * nums.back()) % MOD;
+        }
+        nums.pop_back();
+    }
+    return ans;
+}
+
+ll divmod(ll num, ll denom, ll MOD) {
+    return (num * exp(denom, MOD-2, MOD)) % MOD;
+}
